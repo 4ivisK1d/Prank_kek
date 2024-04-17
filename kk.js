cef749895703c7aeebeb1e9450f28ffd6d7e901e
4ivisK1d <lockio.com@gmail.com>
2024-04-17 19:43:15 +0300

    Initial commit

---
+++ kk.js
@@ -0,0 +1,9 @@
+document.getElementById("btn-no").addEventListener("click", function () {
+  document.location = "/prank.html";
+});
+
+const msovr = document.getElementById("btn-yes");
+msovr.addEventListener("mouseover", function () {
+  msovr.style.left = `${0 + Math.random() * (window.innerWidth + 1 - 110)}px`;
+  msovr.style.top = `${0 + Math.random() * (window.innerHeight + 1 - 110)}px`;
+});
